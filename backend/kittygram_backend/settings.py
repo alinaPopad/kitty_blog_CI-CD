03157b817e5be93b23173b4b332080fc1bce97ed
Алина Попадченко <popadchenko.a@gmail.com>
2023-09-09 23:27:56 +0300

    review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-SECRET_KEY=os.getenv('SECRET_KEY')
+SECRET_KEY=os.getenv('SECRET_KEY', default='*')
 
 DEBUG = os.getenv('DEBUG', default=False) == 'True'
 
